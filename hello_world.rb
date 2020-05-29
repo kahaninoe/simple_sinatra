370c7cbfbb683f7d894901981a6116f2b526fb67
Ren Provey <ren@renprovey.com>
2020-05-29 15:39:36 -0400

    Add basic ERB example.

--- hello_world.rb
+++ hello_world.rb
@@ -5,3 +5,11 @@ require 'byebug'
 get '/' do
   'Hello world!'
 end
+
+get '/erb_example' do
+  hi = "I'm a (local) variable that won't get passed to the view"
+  @hi = "I'm a (instance) variable that will get passed to the view!"
+  @message = "Welcome to COMPUTER SCIENCE WEBSITE"
+
+  erb :index
+end
