da383fc4cb8b3867eccca49c38fa95b4a9034873
Ren Provey <ren@renprovey.com>
2020-06-01 08:12:48 -0400

    Add Character class

--- hello_world.rb
+++ hello_world.rb
@@ -13,3 +13,15 @@ get '/erb_example' do
 
   erb :index
 end
+
+class Character
+  # attr_accessor saves you from getter and setter methods! https://www.rubyguides.com/2018/11/attr_accessor/
+  attr_accessor :name, :favorite_quote
+
+  # https://www.rubyguides.com/2019/01/ruby-initialize-method/
+  def initialize(name, favorite_quote)
+    # we need to convert the variables from local to instance
+    @name = name
+    @favorite_quote = favorite_quote
+  end
+end
